e65fe46c959c506bcd17d8b9fc3c05ab27abef2b
Marfur <Marfur@marfur.com>
2022-12-01 01:02:10 +0000

    Remove unneeded code

--- src/app/terminal-prompt/terminal-prompt.component.ts
+++ src/app/terminal-prompt/terminal-prompt.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fab, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-terminal-prompt',
@@ -6,64 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./terminal-prompt.component.scss'],
 })
 export class TerminalPromptComponent implements OnInit {
-  private name = 'Adam Duboczky';
-  private job = 'Software Developer';
-  private employment = 'Synectics';
-
-  public nameText = '';
-  public jobText = '';
-  public employmentText = '';
-
-  public nameCursor = true;
-  public jobCursor = true;
-  public employmentCursor = true;
-
-  constructor() {}
+  constructor(private library: FaIconLibrary) {}
 
   ngOnInit(): void {
-    this.populateText(this.name, (name) => this.setName(name));
-    this.populateText(this.job, (job) => this.setJob(job));
-    this.populateText(this.employment, (employment) =>
-      this.setEmployment(employment)
-    );
-  }
-
-  populateText = (
-    word: string,
-    callback: (word: string) => void,
-    index = 0
-  ) => {
-    if (word.length === index - 1) {
-      return;
-    }
-
-    callback(word.slice(0, index));
-    setTimeout(() => {
-      this.populateText(word, callback, index + 1);
-    }, 200);
-  };
-
-  setName(character: string) {
-    this.nameText = character;
-  }
-
-  getNameCursor(): boolean {
-    return this.name !== this.nameText;
-  }
-
-  setJob(character: string) {
-    this.jobText = character;
-  }
-
-  getJobCursor(): boolean {
-    return this.job !== this.jobText;
-  }
-
-  setEmployment(character: string) {
-    this.employmentText = character;
-  }
-
-  getEmploymentCursor(): boolean {
-    return this.employment !== this.employmentText;
+    this.library.addIcons(faGithub, faLinkedin);
   }
 }
